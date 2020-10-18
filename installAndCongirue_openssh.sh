9a5a69493f8be4d9552f1232bf9653f4b784dde4
choisungwook <kgg1959@naver.com>
2020-10-18 22:17:13 +0900

    Change vagrant configure script: useradd 설정 추가(홈 디렉터리, 로그인 쉘)

--- installAndCongirue_openssh.sh
+++ installAndCongirue_openssh.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-useradd test
+useradd -m -s /bin/bash test  # -m: home directory, -s: login shell
+useradd -m -s /bin/bash test2  # -m: home directory, -s: login shell
 echo -e "toor\ntoor" | passwd test
+echo -e "toor\ntoor" | passwd test2
 echo -e "toor\ntoor" | passwd # root password
 sed  -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service ssh restart
